2cbca2ef449906f4a72f276856c36201515d4b73
Liam Brand <liamtbrand@gmail.com>
2019-08-31 12:45:09 +1200

    Improve hurling

--- pyook.py
+++ pyook.py
@@ -8,35 +8,26 @@ def hurl(sick, bucket):
         pyook += bucket[chunk]
         if chunk == " ":
             pyook += "py"
-    return pyook+"k"
+        else:
+            pyook += "0"
+    return pyook[:-1]+"k"
+
+def pour(pyook, bucket):
+    pyook = re.sub(r"([pP][yY])|([0]*[kK])", lambda x: '', pyook)
+    return "".join([bucket[chunk] for chunk in re.compile(r"[0]+").split(pyook)])
 
 def empty(pyook, bucket):
     bucket = {v:k for k,v in bucket.items()}
-    ook = "".join(pyook.split("py"))
-    ook = re.sub(r"[k]{1}", lambda x: '0k ', ook)
-    ook = re.sub(r"\s+[0]+", lambda x: ' ', ook)
-    ook = re.sub(r"[0]{2,}[k]\s+", lambda x: '0k ', ook)
-    sick = ""
-    while len(ook) != 0:
-        for chunk in sorted(bucket.keys(),reverse=True):
-            if ook.startswith(chunk):
-                ook = ook[len(chunk):]
-                sick += bucket[chunk]
-                break
-    return sick
+    return re.sub(r"([pP][yY])?[O0o]+[kK]", lambda x: pour(x.group(0), bucket), pyook)
 
 if __name__ == "__main__":
     if len(sys.argv) < 2:
         print("Ni! Gimme PyOok!")
     else:
-        pyook = ""
-        for word in sys.argv[1:]:
-            pyook += word + " "
-        pyook = pyook.strip()
-        print("'"+pyook+"'")
-        if re.match(r"^(([O0o]+)[kK]\s*)+$", pyook):
+        pyook = " ".join(sys.argv[1:]).strip()
+        if re.match(r"^(([pP][yY])?[O0o]+[kK]\s*)+$", pyook):
             print(empty(pyook, bucket.get()))
-        elif re.match(r"^([pP][yY](\w+)[kK]\s*)+$", pyook):
+        elif re.match(r"^(([pP][yY])?\w+[kK]\s*)+$", pyook):
             print("I don't like the PyOok.")
         else:
             print(hurl(pyook.lower(), bucket.get()))
